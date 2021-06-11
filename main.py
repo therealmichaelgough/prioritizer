5f5154aea6283ecd32ac34b0a628e1f053622d73
therealmichaelgough <mdgough12@gmail.com>
2021-06-11 00:53:50 -0700

    complete

--- main.py
+++ main.py
@@ -4,11 +4,34 @@ from datetime import timedelta
 import datetime
 import click
 import re
+from word2number import w2n
 
 TASK_FILE_NAME = 'tasks.csv'
 CALENDAR_FILE_NAME = f'tasks_{arrow.now().date}'
-MIN_INTERVAL = timedelta(minutes=20)
-ESTIMATION_PATTERN = re.compile()
+MIN_ESTIMATION_QUANTITY = 20
+MIN_ESTIMATION_UNIT = 'minutes'
+MAX_ESTIMATION_UNIT = 'years'
+MIN_INTERVAL = timedelta(**{MIN_ESTIMATION_UNIT: MIN_ESTIMATION_QUANTITY})
+ESTIMATION_PATTERN = re.compile(r'(?P<quantity>((\d+|\d+\/\d+)|[a-zA-Z]+)) +(?P<unit>(days?|min(ute)?s?)|weeks?|years?|hours?|months?|lifetime)')
+
+
+def parse_quantity(quantity):
+    try:
+        if quantity == 'a':
+            return 1
+        try:
+            return float(quantity)
+        except ValueError:
+            try:
+                if '/' in quantity:
+                    numerator, denominator = quantity.split('/')[0], quantity.split('/')[1]
+                    return float(numerator) / float(denominator)
+                else:
+                    raise
+            except:
+                raise
+    except:
+        return Task.extract_quantity_from_words(quantity)
 
 
 @total_ordering
@@ -23,15 +46,22 @@ class Task:
         self.critical = kwargs.get('critical', False)
         self.time_estimate = kwargs.get('time_estimate', MIN_INTERVAL)
         self.remaining_estimate = kwargs.get('remaining_estimate', self.time_estimate)
-        self.due_date = kwargs.get('due_date')
+        self.due_date = kwargs.get('due_date', 'in 1 year')
         self.dependencies = [Task(**dependency.__dict__) for dependency in kwargs.get('dependencies', [])]
         self.priority = self.assign_priority()
 
+    def __repr__(self):
+        return f'\nTask: {self.name}\n\tDescription: {self.description}\n\tDue: {self.due_date}\n\tTime Remaining: {self.remaining_estimate}'
+
+    def __str__(self):
+        return self.__repr__()
+
     def assign_priority(self):
-        critical = {True: lambda x: -1 * float(x), False: lambda x: 1/float(x)}[self.critical]
-        time_cost = float(self.time_estimate / MIN_INTERVAL)
-        available_time_cost = float((self.due_date - arrow.now()) / MIN_INTERVAL)
-        return critical(time_cost/available_time_cost)
+        relative_remaining_time = float((self.due_date.ceil('day') - arrow.now()) / self.remaining_estimate)
+        critical_modifier = {True: -1, False: 1}[self.critical]
+        if relative_remaining_time <= 0:
+            critical_modifier *= -1
+        return critical_modifier * relative_remaining_time
 
     def __eq__(self, other):
         return all([self.name == other.name,
@@ -46,11 +76,34 @@ class Task:
 
     @classmethod
     def parse_time_delta(cls, task_estimation_string):
-        return None
+        match = ESTIMATION_PATTERN.match(task_estimation_string)
+        if not match:
+            quantity = MIN_ESTIMATION_QUANTITY
+        else:
+            quantity = match.groupdict().get('quantity', MIN_ESTIMATION_QUANTITY)
+            quantity = parse_quantity(quantity)
 
+        unit = match.groupdict().get('unit', MIN_ESTIMATION_UNIT)
+
+        if unit in ['minutes', 'hours', 'days', 'weeks', 'months', 'years']:
+            pass
+        elif unit in ['minute', 'hour', 'day', 'week', 'month', 'year']:
+            unit = f'{unit}s'
+        elif unit in ['min', 'mins']:
+            unit = 'minutes'
+        elif unit in ['lifetime']:
+            return timedelta(days=365 * 40)  # tick tick tick tick
+        else:
+            unit = MIN_ESTIMATION_UNIT
+
+        return timedelta(**{unit: quantity})
 
     @classmethod
     def parse_due_date(cls, task_due_date_string):
+        if task_due_date_string in ['today', 'now']:
+            return arrow.get()
+        elif task_due_date_string in ['tomorrow']:
+            return arrow.get().shift(days=1).ceil('day')
         try:
             try:
                 return arrow.get(task_due_date_string, 'MM-DD-YYYY')
@@ -58,18 +111,28 @@ class Task:
                 return arrow.get(f'{task_due_date_string}-{arrow.get().year}', 'MM-DD-YYYY')
         except:
             try:
-                return None
+                for word_number in w2n.american_number_system.keys():
+                    if word_number in task_due_date_string:
+                        task_due_date_string = task_due_date_string.replace(word_number, str(w2n.american_number_system[word_number]))
+                return arrow.get().dehumanize(task_due_date_string)
             except:
                 return None
 
+    @classmethod
+    def extract_quantity_from_words(cls, quantity):
+        try:
+            return w2n.word2number(quantity)
+        except:
+            return MIN_ESTIMATION_QUANTITY
+
 
 class Prioritizer:
-    def __init__(self, *tasks):
+    def __init__(self, tasks):
         self.queue = tasks if tasks else []
 
-    def insert(self, *tasks):
+    def insert(self, tasks):
         for task in tasks:
-            if type(task) is Task:
+            if task.__class__ is Task:
                 self.queue.append(task)
 
     def output_calendar(self, filename):
@@ -81,20 +144,23 @@ class Prioritizer:
     def sorted_tasks(self):
         return sorted(self.queue)
 
+    def print_sorted_tasks(self):
+        click.clear()
+        print(f'\n{"-"*80}\n'.join([f'{str(t)}\n' for t in self.sorted_tasks()]))
+
 
 class CliParser:
     @staticmethod
-    @click.command()
     def new_task_from_prompt():
         task_name = click.prompt("What do you need to do? ")
         if not task_name or task_name in ["nothing", "done", "\n"]:
-            return False
+            return None
         task_description = click.prompt("Short Description: ")
         task_critical = {"yes": True, "no":False}[click.prompt("Is this a critical task? ",
                                      type=click.Choice(["yes", "no"], case_sensitive=False),
                                      show_choices=True)]
-        task_estimation = click.prompt("How long will this take? ")
-        task_due_date = click.prompt("When is it due? ")
+        task_estimation = click.prompt("How long will this take? e.g. 'one week' | 1/2 day | 20 mins | 1 hour ")
+        task_due_date = click.prompt("When is it due? e.g. 'tomorrow' | 'in 8 days' | 12-23-2021 | 12-23 | today")
         return {
             "name": task_name,
             "description": task_description,
@@ -103,22 +169,23 @@ class CliParser:
             "due_date": Task.parse_due_date(task_due_date)
         }
 
-
     @staticmethod
-    @click.command()
-    def tasks_from_prompt(existing_tasks, new_tasks):
+    def tasks_from_prompt(existing_tasks):
         add_new_task = True
-        existing_tasks_display = "\n\t*".join(existing_tasks)
-        click.echo(f'Here are your existing tasks: {existing_tasks_display}')
+        #existing_tasks_display = "\n\t*".join(existing_tasks)
+        #click.echo(f'Here are your existing tasks: {[Task(t) for t in existing_tasks]}')
         while add_new_task:
             add_new_task = CliParser.new_task_from_prompt()
-            yield add_new_task
+            if add_new_task:
+                yield add_new_task
+            else:
+                return
 
 
 def gather_instructions(existing_tasks):
     new_tasks = []
-    for t in CliParser.tasks_from_prompt(existing_tasks, new_tasks):
-        new_tasks.append(Task(t))
+    for t in CliParser.tasks_from_prompt(existing_tasks):
+        new_tasks.append(Task(**t))
 
     return new_tasks
 
@@ -128,8 +195,6 @@ def load_tasks_from_file(filename):
     return tasks
 
 
-
-
 def main():
     existing_tasks = load_tasks_from_file(TASK_FILE_NAME)
     prioritizer = Prioritizer(existing_tasks)
@@ -137,3 +202,8 @@ def main():
     prioritizer.insert(new_tasks)
     prioritizer.output_calendar(CALENDAR_FILE_NAME)
     prioritizer.output_csv(TASK_FILE_NAME)
+    prioritizer.print_sorted_tasks()
+
+
+if __name__ == "__main__":
+    main()
